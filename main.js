d979eea391b1ada6c3085bc2584f6ed3d8785493
Wesley <wesleyriancsilva@gmail.com>
2023-11-03 18:58:11 -0300

    nome

---
+++ main.js
@@ -0,0 +1,22 @@
+console.log("Esta funcionando...")
+
+const painel1 = document.querySelector("#par-ou-impar")
+const painel2 = document.querySelector("#escolha-numeros")
+const resultado = document.querySelector("#resultado")
+
+function escolherParOuImpar(parOuImpar) {
+    this.parOuImpar = parOuImpar
+    console.log(parOuImpar)
+    painel1.style.display = 'none'
+    painel2.style.display = 'block'
+    return parOuImpar
+    
+}
+
+function escolherNumero(numeroUsuario) {
+    this.numeroUsuario = numeroUsuario
+    console.log(numeroUsuario)
+
+    return numeroUsuario
+
+}
