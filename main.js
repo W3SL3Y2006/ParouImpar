5483a90a19d8f2c38fa4146033e66ebe51c9b19a
Wesley <wesleyriancsilva@gmail.com>
2023-11-10 18:54:31 -0300

    definindo numero do computador

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ console.log("Esta funcionando...")
 const painel1 = document.querySelector("#par-ou-impar")
 const painel2 = document.querySelector("#escolha-numeros")
 const resultado = document.querySelector("#resultado")
+var numeroComputador 
 
 function escolherParOuImpar(parOuImpar) {
     this.parOuImpar = parOuImpar
@@ -30,5 +31,14 @@ function desabilitarBotoes() {
 function selecionar (id) {
     const botao = document.getElementById (id)
     botao.classList.add('selecionado')
-    
 }
+
+function escolherNumeroComputador() {
+    numeroComputador = Math.floor (Math.random() *6)
+    console.log(numeroComputador)
+}
+
+function jogarParOuImpar () {
+    console.log('Chamou a função jogar...')
+}
+
